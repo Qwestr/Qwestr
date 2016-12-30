e89dbbe9cec54158848acf9d1dd2ab6d457e3afe
Shawn Daichendt <shawn.daichendt@gmail.com>
2016-12-30 15:19:57 -0500

    added Firebase initialization code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
-
+import firebase from 'firebase';
+import firebaseConfig from './config/firebase.json';
 import Routes from './routes';
-
 import './index.css';
 
+// initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Render React DOM
 ReactDOM.render(
   <Routes history={browserHistory} />,
   document.getElementById('root')
