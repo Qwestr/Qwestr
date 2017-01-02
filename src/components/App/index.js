98e7f92e420e8fa112eff346ca90f726ea5aa871
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-01-02 15:47:49 -0500

    added firebaseui/ react-bootstrap packages, Facebook login

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import classnames from 'classnames';
-
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './style.css';
 
 class App extends Component {
+  loginWithFacebook() {
+    // Setup Firebase Facebook Auth Provider
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      console.log('LOGIN SUCCESS!');
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      console.log('token: ' + token);
+      // The signed-in user info.
+      var user = result.user;
+      console.log('user: ' + user);
+    }).catch(function(error) {
+      console.log('LOGIN ERROR!');
+      // Handle Errors here.
+      var errorCode = error.code;
+      console.log('errorCode: ' + errorCode);
+      var errorMessage = error.message;
+      console.log('errorMessage: ' + errorMessage);
+      // The email of the user's account used.
+      var email = error.email;
+      console.log('email: ' + email);
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.log('credential: ' + credential);
+    });
+  }
+
   render() {
     const { className, ...props } = this.props;
     return (
@@ -16,6 +45,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button bsStyle="primary" onClick={this.loginWithFacebook}>Login with Facebook</Button>
       </div>
     );
   }
