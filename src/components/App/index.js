43352bb30789500ea337a10ea5a9239f90c9e100
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-01-10 16:43:22 -0500

    added logic to navbar to display items depending on authenticated user status

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import firebase from 'firebase';
 import classnames from 'classnames';
 import './style.css';
 
 class App extends Component {
+  constructor(props) {
+    // set props
+    super(props);
+
+    // set state
+    this.state = {
+      user: null
+    };
+  }
+
+  getNavbarItems(user) {
+    // return items depending on if the user is logged in or not
+    if (user) {
+      return (
+        <div className="App-navbar-items">
+          <Nav>
+            <NavItem href="/qwest/new">New Qwest</NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem href="/logout">Logout</NavItem>
+          </Nav>
+      </div>
+      );
+    } else {
+      return (
+        <div className="App-navbar-items">
+          <Nav pullRight>
+            <NavItem href="/login">Login</NavItem>
+          </Nav>
+        </div>
+      );
+    }
+  }
+
+  watchUserState() {
+    // setup listener
+    firebase.auth().onAuthStateChanged((user) => {
+      // set the user
+      this.setState({user: user});
+    });
+  }
+
+  componentDidMount() {
+    // setup listener for user changes
+    this.watchUserState();
+  }
+
   render() {
     // declare relevant properties as local variables
     const { className, children, ..._props } = this.props;
@@ -18,13 +66,7 @@ class App extends Component {
                 <a href="/">Qwestr</a>
               </Navbar.Brand>
             </Navbar.Header>
-            <Nav>
-              <NavItem href="/qwest/new">New Qwest</NavItem>
-            </Nav>
-            <Nav pullRight>
-              <NavItem href="/login">Login</NavItem>
-              <NavItem href="/logout">Logout</NavItem>
-            </Nav>
+            {this.getNavbarItems(this.state.user)}
           </Navbar>
         </div>
         <div className="App-content">
