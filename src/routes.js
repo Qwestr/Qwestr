057828c6441b3d3021d677dedc3403e1667d0f6d
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-01-13 14:01:24 -0500

    created Home page component/ updated routes/ minor updates to other stylesheets

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 import App from './components/App';
+import Home from './components/Home';
 import Login from './components/Login';
 import Qwest from './components/Qwest';
 import NotFound from './components/NotFound';
@@ -8,6 +9,7 @@ import NotFound from './components/NotFound';
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={App}>
+      <IndexRoute component={Home} />
       <Route path="login" component={Login} />
       <Route path="qwest/new" component={Qwest} />
       <Route path="*" component={NotFound} />
