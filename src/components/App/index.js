40833f3e56eacf0d6e5bc368db8415c02a81e17b
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-01-16 00:15:36 -0500

    integrated new NavBar component into App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,59 +1,9 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
-import firebase from 'firebase';
 import classnames from 'classnames';
-import { logout } from '../../lib/auth';
+import NavBar from '../NavBar';
 import './style.css';
 
 class App extends Component {
-  constructor(props) {
-    // set props
-    super(props);
-
-    // set state
-    this.state = {
-      user: null
-    };
-  }
-
-  getNavbarItems(user) {
-    // return items depending on if the user is logged in or not
-    if (user) {
-      return (
-        <div className="App-navbar-items">
-          <Nav>
-            <NavItem href="/qwest/list">Qwests</NavItem>
-            <NavItem href="/qwest/new">New Qwest</NavItem>
-          </Nav>
-          <Nav pullRight>
-            <NavItem onClick={logout}>Logout</NavItem>
-          </Nav>
-      </div>
-      );
-    } else {
-      return (
-        <div className="App-navbar-items">
-          <Nav pullRight>
-            <NavItem href="/login">Login</NavItem>
-          </Nav>
-        </div>
-      );
-    }
-  }
-
-  watchAuthState() {
-    // setup auth change listener
-    firebase.auth().onAuthStateChanged((user) => {
-      // set the state
-      this.setState({user: user});
-    });
-  }
-
-  componentDidMount() {
-    // setup listeners
-    this.watchAuthState();
-  }
-
   render() {
     // declare relevant properties as local variables
     const { className, children, ..._props } = this.props;
@@ -61,16 +11,7 @@ class App extends Component {
     // render the veiw
     return (
       <div className={classnames('App', className)}>
-        <div className="App-navbar">
-          <Navbar inverse>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">Qwestr</a>
-              </Navbar.Brand>
-            </Navbar.Header>
-            {this.getNavbarItems(this.state.user)}
-          </Navbar>
-        </div>
+        <NavBar />
         <div className="App-content">
           {children}
         </div>
