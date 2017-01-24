dbc9dff2ec6fc75ded3304e8476129dc53bdcc8f
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-01-24 13:14:06 -0500

    implemented assignQwest() method

--- src/components/Qwest/List/index.js
+++ src/components/Qwest/List/index.js
@@ -16,6 +16,7 @@ import {
 } from 'react-bootstrap';
 import {
   getUserQwests,
+  acceptQwest,
   completeQwest,
   restartQwest,
   assignQwest,
@@ -256,6 +257,7 @@ class QwestList extends Component {
   }
 
   getAssignedQwestList() {
+    console.log('this.state.qwests.assigned: ' + JSON.stringify(this.state.qwests.assigned));
     if (this.state.qwests && this.state.qwests.assigned) {
       return Object.keys(this.state.qwests.assigned).map((key) =>
         <ListGroupItem key={key}>
@@ -315,7 +317,7 @@ class QwestList extends Component {
       <ButtonGroup className="Qwest-item-button-group">
         <Button
           bsStyle="primary"
-          // onClick={() => acceptQwest(key)}
+          onClick={() => acceptQwest(qwest, key)}
         >
           Accept
         </Button>
