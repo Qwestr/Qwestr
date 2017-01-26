cd46ee485f60ef23e7f7645ef71470f4bc00927d
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-01-26 14:47:22 -0500

    updated createQwest() method and tests

--- src/lib/__tests__/qwest.js
+++ src/lib/__tests__/qwest.js
@@ -1,3 +1,23 @@
+import firebase from 'firebase';
+import firebaseConfig from '../../config/firebase.json';
+import { createQwest } from '../qwest';
+
+// initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 it('works', () => {
+  // Create User data object
+  const userData = {
+    uid: 'test'
+  };
+
+  // Create Qwest data object.
+  const qwestData = {
+    title: 'Test'
+  };
+  createQwest(userData, qwestData, function(data) {
+    console.log('works!');
+  })
+
   expect(true);
 });
--- src/lib/qwest.js
+++ src/lib/qwest.js
@@ -1,8 +1,8 @@
 import firebase from 'firebase';
 
-export function createQwest(qwestData, successCallback) {
-  // Get current user id
-  const userId = firebase.auth().currentUser.uid;
+export function createQwest(userData, qwestData, successCallback) {
+  // Get user id
+  const userId = userData.uid;
 
   // Get a key for a new Qwest.
   var qwestKey = firebase.database().ref().child('qwests').push().key;
