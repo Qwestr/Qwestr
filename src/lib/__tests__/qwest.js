f832aeed5eb2a57e1ef76c2b1560c4345730d3b3
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-02-01 13:04:40 -0500

    created test for restartQwest()

--- src/lib/__tests__/qwest.js
+++ src/lib/__tests__/qwest.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
-import { createQwest, completeQwest } from '../qwest';
+import { createQwest, completeQwest, restartQwest } from '../qwest';
 
 afterEach(() => {
   firebase.__clearMockDatabase();
 })
 
-it('creates a Qwest', () => {
+it('successfully creates a Qwest', () => {
   // Get authorized User ID
   const currentAuthUserId = firebase.__getAuthUserId();
 
@@ -20,7 +20,7 @@ it('creates a Qwest', () => {
   // Get resulting database
   const database = firebase.__getMockDatabase();
 
-  // Expect that the new Qwest has been created with the proper information
+  // Expect that the Qwest has been created with the proper information
   expect(Object.keys(database['qwests'])).toHaveLength(1);
   expect(database['qwests']['mockId1'].title).toBe('Test Qwest');
   expect(database['qwests']['mockId1'].createdBy).toBe(currentAuthUserId);
@@ -32,7 +32,7 @@ it('creates a Qwest', () => {
   expect(database['user-qwests'][currentAuthUserId]['active']['mockId1'].title).toBe('Test Qwest');
 });
 
-it('completes a Qwest', () => {
+it('successfully completes a Qwest', () => {
   // Get authorized User ID
   const currentAuthUserId = firebase.__getAuthUserId();
 
@@ -50,7 +50,7 @@ it('completes a Qwest', () => {
   // Get resulting database
   const database = firebase.__getMockDatabase();
 
-  // Expect that the new Qwest has been created and completed
+  // Expect that the Qwest has been created and completed
   expect(Object.keys(database['qwests'])).toHaveLength(1);
   expect(database['qwests']['mockId1'].completed).toBeTruthy();
 
@@ -60,4 +60,37 @@ it('completes a Qwest', () => {
   expect(Object.keys(database['user-qwests'][currentAuthUserId])).toHaveLength(1);
   expect(Object.keys(database['user-qwests'][currentAuthUserId]['completed'])).toHaveLength(1);
   expect(database['user-qwests'][currentAuthUserId]['completed']['mockId1'].title).toBe('Test Qwest');
+});
+
+it('successfully restarts a Qwest', () => {
+  // Get authorized User ID
+  const currentAuthUserId = firebase.__getAuthUserId();
+
+  // Create Qwest data object.
+  const qwestData = {
+    title: 'Test Qwest'
+  };
+
+  // Create the Qwest
+  createQwest(qwestData);
+
+  // Complete the Qwest
+  completeQwest(qwestData, 'mockId1');
+
+  // Rdstart the Qwest
+  restartQwest(qwestData, 'mockId1');
+
+  // Get resulting database
+  const database = firebase.__getMockDatabase();
+
+  // Expect that the Qwest has been created, and is no longer marked as completed
+  expect(Object.keys(database['qwests'])).toHaveLength(1);
+  expect(database['qwests']['mockId1'].completed).toBeFalsy();
+
+  // Expect that the Completed User Qwest has been removed,
+  // and the Active User Qwest has been created
+  expect(Object.keys(database['user-qwests'])).toHaveLength(1);
+  expect(Object.keys(database['user-qwests'][currentAuthUserId])).toHaveLength(1);
+  expect(Object.keys(database['user-qwests'][currentAuthUserId]['active'])).toHaveLength(1);
+  expect(database['user-qwests'][currentAuthUserId]['active']['mockId1'].title).toBe('Test Qwest');
 });
