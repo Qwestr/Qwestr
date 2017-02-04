32e64d4d562bc5fa783e867dd60a88622b0ac0a1
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-02-04 02:22:22 -0500

    updated path to firebaseConfig

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
 import firebase from 'firebase';
-import firebaseConfig from './config/firebase.json';
+import firebaseConfig from '../firebase.json';
 import Routes from './routes';
 import './index.css';
 
