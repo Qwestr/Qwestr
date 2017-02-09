33b174efbf5d72321b1c21b15d805ecafdb12189
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-02-09 12:51:08 -0500

    added test for getAll() method

--- src/models/UserQwestList/__tests__/index.js
+++ src/models/UserQwestList/__tests__/index.js
@@ -1,3 +1,26 @@
-it('works', () => {
-  expect(true);
-});
+import firebase from 'firebase'
+import Qwest from '../../Qwest'
+import UserQwestList from '../'
+
+afterEach(() => {
+  firebase.__resetAuthUserId()
+  firebase.__clearMockDatabase()
+})
+
+it('successfully returns a list of User Qwests', () => {
+  // Create Qwest object and save
+  const qwest = new Qwest({
+    title: 'Test Qwest'
+  })
+
+  qwest.create()
+
+  // Create User Qwest List and get all User Qwests
+  const userQwestList = new UserQwestList()
+
+  userQwestList.getAll(() => {})
+
+  // Expect that the correct list of User Qwests has been returned
+  expect(Object.keys(userQwestList)).toHaveLength(1)
+  expect(Object.keys(userQwestList['active'])).toHaveLength(1)
+})
