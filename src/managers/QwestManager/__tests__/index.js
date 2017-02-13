2f74cc324fcb55c721e7991ab23bd99c9bb4d2fa
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-02-13 14:03:36 -0500

    added restart() test

--- src/managers/QwestManager/__tests__/index.js
+++ src/managers/QwestManager/__tests__/index.js
@@ -62,3 +62,34 @@ it('successfully completes a Qwest', () => {
   expect(userQwests.completed).toBeTruthy()
   expect(userQwests.completed['mockId1'].title).toBe(qwest.title)
 })
+
+it('successfully restarts a Qwest', () => {
+  // Create a new Qwest
+  const qwest = createNewQwest()
+
+  // Create an initial User Qwests object
+  let userQwests = {}
+
+  // Create a new QwestManager and get all User Qwests
+  const qwestManager = new QwestManager()
+  qwestManager.getAllUserQwests((data) => {
+    userQwests = data
+  })
+
+  // Complete the Qwest
+  qwestManager.complete('mockId1')
+
+  // Restart the Qwest
+  qwestManager.restart('mockId1')
+
+  // Get resulting database
+  const database = firebase.__getMockDatabase()
+
+  // Expect that the correct Qwest data has been created/ updated
+  expect(database['qwests']['mockId1'].completed).toBeFalsy()
+
+  // Expect that the correct User Qwest data has been created/ updated
+  expect(userQwests.active).toBeTruthy()
+  expect(userQwests.completed).toBeFalsy()
+  expect(userQwests.active['mockId1'].title).toBe(qwest.title)
+})
