d6e48c0da39ac92cec0431a8ab690b41b73d5dc5
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-02-18 22:29:59 -0500

    updated User model

--- src/models/User/index.js
+++ src/models/User/index.js
@@ -1,29 +1,35 @@
 export class SocialUser {
   constructor(props) {
-    this.uid = props.uid
+    this.uid = props.uid || null
   }
 }
 
 export default class User {
   constructor(props) {
-    this.displayName = props.displayName
-    this.photoURL = props.photoURL
-    this.credentials = props.credentials || {}
+    this.displayName = props.displayName || null
+    this.photoURL = props.photoURL || null
+    this.credentials = props.credentials || null
   }
 
   update(userData) {
-    this.displayName = userData.displayName
-    this.photoURL = userData.photoURL
+    this.displayName = userData.displayName || null
+    this.photoURL = userData.photoURL || null
   }
 
   updateCredentials(providerData, credentials) {
     for (const provider of providerData) {
       if (credentials.provider===provider.providerId && provider.providerId==='facebook.com') {
+        if (!this.credentials) {
+          this.credentials = {}
+        }
         this.credentials['Facebook'] = {
           id: provider.uid,
           accessToken: credentials.accessToken
         }
       } else if (credentials.provider===provider.providerId && provider.providerId==='google.com') {
+        if (!this.credentials) {
+          this.credentials = {}
+        }
         this.credentials['Google'] = {
           id: provider.uid,
           accessToken: credentials.accessToken
