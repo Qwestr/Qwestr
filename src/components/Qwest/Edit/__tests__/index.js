105caae70f0412b1b1ad36e5b798ba2caec2eef1
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-03-20 15:23:13 -0400

    created QwestEdit component

---
+++ src/components/Qwest/Edit/__tests__/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import QwestEdit from '../'
+
+it('renders without crashing', () => {
+  shallow(<QwestEdit />)
+})
---
+++ src/components/Qwest/Edit/index.js
@@ -0,0 +1,157 @@
+import firebase from 'firebase'
+import classnames from 'classnames'
+import React, { Component } from 'react'
+import { Button, Col, ControlLabel, Form, FormControl, FormGroup, HelpBlock, Panel } from 'react-bootstrap'
+import { browserHistory } from 'react-router'
+import Qwest from '../../../models/Qwest'
+import './style.css'
+
+class QwestEdit extends Component {
+  constructor(props) {
+    // Set props
+    super(props)
+
+    // Set state
+    this.state = {
+      title: '',
+      description: '',
+      validationState: {
+        title: null,
+        description: null
+      },
+      validationText: {
+        title: null,
+        description: null
+      }
+    }
+  }
+
+  createQwestSuccessCallback(data) {
+    // Redirect to Qwest list route
+    browserHistory.push('/qwest/list')
+  }
+
+  handleChange(event) {
+    // Update state values
+    if (event.target.id === 'title') {
+      this.setState({title: event.target.value})
+    } else if (event.target.id === 'description') {
+      this.setState({description: event.target.value})
+    }
+  }
+
+  validateForm() {
+    if (!this.state.title) {
+      // Update the state and return false
+      let updatedValidationState = this.state.validationState
+      updatedValidationState.title = 'error'
+
+      let updatedValidationText = this.state.validationText
+      updatedValidationText.title = 'A title is required.'
+
+      this.setState({
+        validationState: updatedValidationState,
+        validationText: updatedValidationText
+      })
+
+      return false
+    }
+
+    return true
+  }
+
+  handleFormSubmit(event) {
+    // Stop the form submission from reloading the page
+    event.preventDefault()
+
+    if (this.validateForm()) {
+      // Create new Qwest object and save
+      const newQwest = new Qwest({
+        title: this.state.title,
+        description: this.state.description
+      })
+
+      newQwest.create(this.createQwestSuccessCallback)
+    }
+  }
+
+  watchAuthState() {
+    // Setup auth change listener
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        // If User has not been authenticated, redirect to home
+        browserHistory.push('/')
+      }
+    })
+  }
+
+  componentDidMount() {
+    // Setup listeners
+    this.watchAuthState()
+  }
+
+  getValidationText(field) {
+    if (this.state.validationText[field]) {
+      return (
+        <HelpBlock>{this.state.validationText[field]}</HelpBlock>
+      )
+    } else {
+      return null
+    }
+  }
+
+  render() {
+    // Declare relevant properties as local variables
+    const { className, ..._props } = this.props
+
+    // Declare other local variables
+    const panelHeader = (<h3>Create New Qwest</h3>)
+
+    // Render the veiw
+    return (
+      <div className={classnames('QwestCreate', className)}>
+        <div className="QwestCreate-content">
+          <Panel header={panelHeader}>
+            <Form horizontal onSubmit={(event) => this.handleFormSubmit(event)}>
+              <FormGroup controlId="title" validationState={this.state.validationState.title}>
+                <Col componentClass={ControlLabel} sm={2}>
+                  Title
+                </Col>
+                <Col sm={10}>
+                  <FormControl
+                    type="text"
+                    placeholder="What this Qwest will be called"
+                    onChange={(event) => this.handleChange(event)}
+                    value={this.state.title}
+                  />
+                  {this.getValidationText('title')}
+                </Col>
+              </FormGroup>
+              <FormGroup controlId="description" validationState={this.state.validationState.description}>
+                <Col componentClass={ControlLabel} sm={2}>
+                  Description
+                </Col>
+                <Col sm={10}>
+                  <FormControl
+                    type="text"
+                    placeholder="What this Qwest is all about"
+                    onChange={(event) => this.handleChange(event)}
+                    value={this.state.description}
+                  />
+                  {this.getValidationText('description')}
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Col smOffset={2} sm={10}>
+                  <Button type="submit">Create</Button>
+                </Col>
+              </FormGroup>
+            </Form>
+          </Panel>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default QwestEdit
