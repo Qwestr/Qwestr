136af4819b11cc323c21be37766cc33fb9085919
Shawn Daichendt <shawn.daichendt@gmail.com>
2017-05-22 16:11:36 -0400

    updated QwestQwick component code/ style

--- src/components/Qwest/Qwick/index.js
+++ src/components/Qwest/Qwick/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import {
-  Button, Col, ControlLabel, Form, FormControl, FormGroup,
-  Grid, ListGroupItem, Row
+  Button, Col, Form, FormControl, FormGroup,
+  Grid, HelpBlock, ListGroupItem, Row
 } from 'react-bootstrap'
+import Qwest from '../../../models/Qwest'
 import './style.css'
 
 export default class QwestQwick extends Component {
@@ -15,21 +16,97 @@ export default class QwestQwick extends Component {
     super(props)
 
     // set state
-    this.state = {}
+    this.state = {
+      title: '',
+      validationState: {
+        title: null,
+      },
+      validationText: {
+        title: null,
+      }
+    }
+  }
+
+  createQwestSuccessCallback() {
+    // Clear form fields
+    this.resetForm()
+  }
+
+  handleChange(event) {
+    // Update state values
+    if (event.target.id === 'title') {
+      this.setState({title: event.target.value})
+    }
+  }
+
+  resetForm() {
+    // Clear form fields
+    this.setState({
+      title: '',
+      validationState: {
+        title: null,
+      },
+      validationText: {
+        title: null,
+      }
+    })
+  }
+
+  validateForm() {
+    if (!this.state.title) {
+      // Update the state and return false
+      let updatedValidationState = this.state.validationState
+      updatedValidationState.title = 'error'
+
+      let updatedValidationText = this.state.validationText
+      updatedValidationText.title = 'A title is required.'
+
+      this.setState({
+        validationState: updatedValidationState,
+        validationText: updatedValidationText
+      })
+
+      return false
+    }
+
+    return true
+  }
+
+  handleFormSubmit(event) {
+    // Stop the form submission from reloading the page
+    event.preventDefault()
+
+    if (this.validateForm()) {
+      // Create new Qwest object and save
+      const newQwest = new Qwest({
+        title: this.state.title
+      })
+
+      newQwest.create(() => this.createQwestSuccessCallback())
+    }
+  }
+
+  getValidationText(field) {
+    if (this.state.validationText[field]) {
+      return (
+        <HelpBlock>{this.state.validationText[field]}</HelpBlock>
+      )
+    } else {
+      return null
+    }
   }
 
   render() {
     return (
       <ListGroupItem>
-        QwestQwick
+        <div className="qwest-qwick-header">
+          Create a Qwest
+        </div>
         <Grid>
           <Row>
             <Form horizontal onSubmit={(event) => this.handleFormSubmit(event)}>
               <FormGroup controlId="title" validationState={this.state.validationState.title}>
-                <Col componentClass={ControlLabel} sm={2}>
-                  Title
-                </Col>
-                <Col sm={10}>
+                <Col sm={12}>
                   <FormControl
                     type="text"
                     placeholder="What this Qwest will be called"
@@ -40,7 +117,7 @@ export default class QwestQwick extends Component {
                 </Col>
               </FormGroup>
               <FormGroup>
-                <Col smOffset={2} sm={10}>
+                <Col sm={12}>
                   <Button type="submit">Create</Button>
                 </Col>
               </FormGroup>
