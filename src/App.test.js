db3cb27df49c8c65d60ee83b9fbc2de75bec0be6
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-09-28 20:31:36 -0400

    Create first unit test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
-import ReactDOM from 'react-dom'
+
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+configure({
+  adapter: new Adapter()
+})
+
+describe('App', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<App></App>)
+  })
+
+  it('should have a form to create a new qwest', () => {
+    expect(wrapper.find('form')).toHaveLength(1)
+  })
 })
