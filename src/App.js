0b3a49da0c2145e865d44bee3eb75a570c52800b
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-09-30 19:38:22 -0400

    Refactored prototype code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react'
 import Aux from 'react-aux'
 
-import QwestForm from './components/QwestForm/QwestForm'
-import QwestList from './components/QwestList/QwestList'
-
 const App = () => {
-  const onSubmit = () => {}
-
-  return (
-    <Aux>
-      <QwestForm onSubmit={onSubmit}></QwestForm>
-      <QwestList></QwestList>
-    </Aux>
-  )
+  return <Aux>Welcome to Qwestr!</Aux>
 }
 
 export default App
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,6 @@ import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
 
-import QwestForm from './components/QwestForm/QwestForm'
-import QwestList from './components/QwestList/QwestList'
 import App from './App'
 
 configure({
@@ -17,11 +15,7 @@ describe('App', () => {
     wrapper = shallow(<App></App>)
   })
 
-  it('should contain a QwestForm component', () => {
-    expect(wrapper.find(QwestForm)).toHaveLength(1)
-  })
-
-  it('should contain a QwestList component', () => {
-    expect(wrapper.find(QwestList)).toHaveLength(1)
+  it('should exist!', () => {
+    expect(wrapper).toBeTruthy()
   })
 })
