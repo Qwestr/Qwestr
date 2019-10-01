1acdb63187a7fe6d9d6699c48c93ba48f7ee877d
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-09-30 20:20:14 -0400

    Working on implementing Dashboard template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import Aux from 'react-aux'
 
+import Dashboard from './Dashboard/Dashboard'
+
 const App = () => {
-  return <Aux>Welcome to Qwestr!</Aux>
+  return (
+    <Aux>
+      <Dashboard></Dashboard>
+    </Aux>
+  )
 }
 
 export default App
