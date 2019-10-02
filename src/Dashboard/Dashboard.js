e51e076dbd96951b59486bbbd4215f8dc9e3d1cf
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-10-02 00:23:40 -0400

    Removed deposits

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -18,7 +18,6 @@ import MenuIcon from '@material-ui/icons/Menu'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import NotificationsIcon from '@material-ui/icons/Notifications'
 import { mainListItems, secondaryListItems } from './listItems'
-import Deposits from './Deposits'
 import Orders from './Orders'
 
 function Copyright() {
@@ -109,9 +108,6 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     overflow: 'auto',
     flexDirection: 'column'
-  },
-  fixedHeight: {
-    height: 240
   }
 }))
 
@@ -124,7 +120,6 @@ export default function Dashboard() {
   const handleDrawerClose = () => {
     setOpen(false)
   }
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight)
 
   return (
     <div className={classes.root}>
@@ -180,18 +175,6 @@ export default function Dashboard() {
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
-            {/* Chart */}
-            {/* <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-                <Chart />
-              </Paper>
-            </Grid> */}
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-                <Deposits />
-              </Paper>
-            </Grid>
             {/* Recent Orders */}
             <Grid item xs={12}>
               <Paper className={classes.paper}>
