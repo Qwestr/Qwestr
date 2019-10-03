6d00b8062ec09c126ed39ef8669afbad31f08547
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-10-02 22:10:36 -0400

    Cleaned up unused code in QwestList

--- src/Dashboard/QwestList.js
+++ src/Dashboard/QwestList.js
@@ -1,23 +1,12 @@
 import React from 'react'
 import Aux from 'react-aux'
-import Avatar from '@material-ui/core/Avatar'
 import IconButton from '@material-ui/core/IconButton'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import ListItemText from '@material-ui/core/ListItemText'
 import Typography from '@material-ui/core/Typography'
 import DeleteIcon from '@material-ui/icons/Delete'
-import FolderIcon from '@material-ui/icons/Folder'
-
-// function generate(element) {
-//   return [0, 1, 2].map(value =>
-//     React.cloneElement(element, {
-//       key: value,
-//     }),
-//   )
-// }
 
 const QwestList = props => {
   // Destructure props
@@ -31,34 +20,14 @@ const QwestList = props => {
       <List>
         {qwests.map((qwest, index) => (
           <ListItem key={index}>
-            <ListItemAvatar>
-              <Avatar>
-                <FolderIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={qwest.name} secondary={index} />
+            <ListItemText primary={qwest.name} />
             <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="delete">
+              <IconButton color="secondary" edge="end" aria-label="delete">
                 <DeleteIcon />
               </IconButton>
             </ListItemSecondaryAction>
           </ListItem>
         ))}
-        {/* {generate(
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <FolderIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary="Single-line item" />
-            <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="delete">
-                <DeleteIcon />
-              </IconButton>
-            </ListItemSecondaryAction>
-          </ListItem>,
-        )} */}
       </List>
     </Aux>
   )
