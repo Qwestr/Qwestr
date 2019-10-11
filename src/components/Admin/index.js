72f5dd9b72ede7ce4b1a3aa96e2113c9465e0b81
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-10-11 00:36:14 -0400

    Added  withAuthorization to Admin page

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import { compose } from 'recompose'
 
+import * as ROLES from '../../constants/roles'
 import { withFirebase } from '../Firebase'
+import { withAuthorization } from '../Session'
 
 class AdminPage extends Component {
   constructor(props) {
@@ -30,6 +33,7 @@ class AdminPage extends Component {
     return (
       <div>
         <h1>Admin</h1>
+        <p>The Admin Page is accessible by every signed in admin user.</p>
         {loading && <div>Loading ...</div>}
         <UserList users={users} />
       </div>
@@ -44,18 +48,24 @@ const UserList = ({ users }) => (
         <span>
           <strong>ID:</strong> {user.id}
         </span>
-        <br></br>
+        <br />
         <span>
           <strong>E-Mail:</strong> {user.data().email}
         </span>
-        <br></br>
+        <br />
         <span>
           <strong>Username:</strong> {user.data().username}
         </span>
-        <br></br>
+        <br />
+        <br />
       </li>
     ))}
   </ul>
 )
 
-export default withFirebase(AdminPage)
+const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN]
+
+export default compose(
+  withAuthorization(condition),
+  withFirebase,
+)(AdminPage)
