90b68afcdf72c1328c3a2c19230ae6e26551a95d
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-10-15 00:14:09 -0400

    Created LoginManagement subcomponent for Account component

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import PasswordChangeForm from '../PasswordChange'
 import { PasswordForgetForm } from '../PasswordForget'
 import { AuthUserContext, withAuthorization } from '../Session'
+
+const SIGN_IN_METHODS = [
+  {
+    id: 'password',
+    provider: null,
+  },
+  {
+    id: 'google',
+    provider: 'googleProvider',
+  },
+  {
+    id: 'facebook',
+    provider: 'facebookProvider',
+  },
+]
+
+class LoginManagement extends Component {
+  render() {
+    return (
+      <div>
+        Sign In Methods:
+        <ul>
+          {SIGN_IN_METHODS.map(signInMethod => {
+            return (
+              <li key={signInMethod.id}>
+                <button type="button" onClick={() => {}}>
+                  {signInMethod.id}
+                </button>
+              </li>
+            )
+          })}{' '}
+        </ul>
+      </div>
+    )
+  }
+}
 
 const AccountPage = () => (
   <AuthUserContext.Consumer>
@@ -11,6 +47,7 @@ const AccountPage = () => (
         <h1>Account: {authUser.email}</h1>
         <PasswordForgetForm />
         <PasswordChangeForm />
+        <LoginManagement authUser={authUser} />
       </div>
     )}
   </AuthUserContext.Consumer>
