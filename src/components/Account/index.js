40e6bd7333c4321122075e00dc41213dd688c5f8
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-10-15 00:40:51 -0400

    Added functionality to link/ unlink login types

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -34,11 +34,29 @@ class LoginManagementBase extends Component {
   }
 
   componentDidMount() {
+    this.fetchSignInMethods()
+  }
+
+  fetchSignInMethods = () => {
     this.props.firebase.auth
       .fetchSignInMethodsForEmail(this.props.authUser.email)
       .then(activeSignInMethods =>
         this.setState({ activeSignInMethods, error: null }),
       )
+      .catch(error => this.setState({ error }))
+  }
+
+  onSocialLoginLink = provider => {
+    this.props.firebase.auth.currentUser
+      .linkWithPopup(this.props.firebase[provider])
+      .then(this.fetchSignInMethods)
+      .catch(error => this.setState({ error }))
+  }
+
+  onUnlink = providerId => {
+    this.props.firebase.auth.currentUser
+      .unlink(providerId)
+      .then(this.fetchSignInMethods)
       .catch(error => this.setState({ error }))
   }
 
@@ -50,16 +68,26 @@ class LoginManagementBase extends Component {
         Sign In Methods:
         <ul>
           {SIGN_IN_METHODS.map(signInMethod => {
+            const onlyOneLeft = activeSignInMethods.length === 1
             const isEnabled = activeSignInMethods.includes(signInMethod.id)
 
             return (
               <li key={signInMethod.id}>
                 {isEnabled ? (
-                  <button type="button" onClick={() => {}}>
+                  <button
+                    type="button"
+                    onClick={() => this.onUnlink(signInMethod.id)}
+                    disabled={onlyOneLeft}
+                  >
                     Deactivate {signInMethod.id}
                   </button>
                 ) : (
-                  <button type="button" onClick={() => {}}>
+                  <button
+                    type="button"
+                    onClick={() =>
+                      this.onSocialLoginLink(signInMethod.provider)
+                    }
+                  >
                     Link {signInMethod.id}
                   </button>
                 )}
