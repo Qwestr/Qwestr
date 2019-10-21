b7ee009e5bd324c545caddaa31ea675e6f84eb66
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-10-20 22:40:03 -0400

    Updated Qwests/ Layout templates/ styles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -112,12 +112,6 @@ const useStyles = makeStyles(theme => ({
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
   },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
 }))
 
 export default function Layout() {
--- src/components/Qwests/index.js
+++ src/components/Qwests/index.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import Aux from 'react-aux'
 import { compose } from 'recompose'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 
 import { withAuthorization, withEmailVerification } from '../Session'
-import QwestList from './list'
 import QwestCreate from './create'
+import QwestList from './list'
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+}))
 
 const QwestsPage = props => {
+  // Load styles
+  const classes = useStyles()
   // Return component
   return (
     <Aux>
-      <QwestCreate store={props.firebase} />
-      <QwestList store={props.firebase} />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <QwestCreate store={props.firebase} />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <QwestList store={props.firebase} />
+          </Paper>
+        </Grid>
+      </Grid>
     </Aux>
   )
 }
