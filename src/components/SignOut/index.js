b98d7b8b190c96ad6ca2a9edd32d7d3f81dcfef2
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-10-24 23:04:08 -0400

    Updated template for SignOutButton

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
 import { withFirebase } from '../Firebase'
 
 const SignOutButton = ({ firebase }) => (
-  <button type="button" onClick={firebase.doSignOut}>
-    Sign Out
-  </button>
+  <Button onClick={firebase.doSignOut}>Sign Out</Button>
 )
 
 export default withFirebase(SignOutButton)
