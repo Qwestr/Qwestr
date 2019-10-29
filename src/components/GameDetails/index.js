3e31836b9a539154e478ebf94f4aac03bdc00023
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-10-29 14:58:57 -0400

    Added useEffect hook to get game in GameDetailsPage

--- src/components/GameDetails/index.js
+++ src/components/GameDetails/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Aux from 'react-aux'
 import { useParams } from 'react-router-dom'
 import { compose } from 'recompose'
@@ -6,14 +6,27 @@ import Typography from '@material-ui/core/Typography'
 
 import { withAuthorization, withEmailVerification } from '../Session'
 
-const GameDetailsPage = () => {
+const GameDetailsPage = props => {
   // Load url params
   const { id } = useParams()
+  // Load state
+  const [game, setGame] = useState(null)
+  // Define effects handlers
+  useEffect(() => {
+    // Setup listener to the game collection object
+    const unsubscribe = props.firebase.game(id).onSnapshot(snapshot => {
+      setGame(snapshot.data())
+    })
+    // Unsubscribe from listener when component is destroyed
+    return () => {
+      unsubscribe()
+    }
+  }, [id, props.firebase])
   // Return component
   return (
     <Aux>
       <Typography variant="h4" gutterBottom>
-        Game Details: {id}
+        Game
       </Typography>
     </Aux>
   )
