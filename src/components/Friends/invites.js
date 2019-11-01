58b9f7710dd0c56c27216afecc0b7ba1feae63fa
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-11-01 18:28:39 -0400

    Added buttons for accepting/ rejecting friend requests

--- src/components/Friends/invites.js
+++ src/components/Friends/invites.js
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from 'react'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardHeader from '@material-ui/core/CardHeader'
+import Grid from '@material-ui/core/Grid'
+import IconButton from '@material-ui/core/IconButton'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
+import CancelIcon from '@material-ui/icons/Cancel'
 
 const SentInviteList = props => {
   // Deconstruct properties
@@ -49,6 +54,14 @@ const ReceivedInviteList = props => {
   const { authUser, firebase } = props
   // Load state
   const [invites, setInvites] = useState([])
+  // Define methods
+  const acceptFriendInvite = invite => {
+    // TODO: put functionality to accept friend invite here
+  }
+
+  const rejectFriendInvite = invite => {
+    // TODO: put functionality to reject friend invite here
+  }
   // Define effects handlers
   useEffect(() => {
     // Setup listener to the invites collection
@@ -74,6 +87,30 @@ const ReceivedInviteList = props => {
                 primary={invite.data().requesterUsername}
                 secondary={invite.data().requesterEmail}
               />
+              <ListItemSecondaryAction>
+                <Grid container spacing={3}>
+                  <Grid item>
+                    <IconButton
+                      color="primary"
+                      edge="end"
+                      aria-label="accept"
+                      onClick={() => acceptFriendInvite(invite)}
+                    >
+                      <CheckCircleIcon />
+                    </IconButton>
+                  </Grid>
+                  <Grid item>
+                    <IconButton
+                      color="secondary"
+                      edge="end"
+                      aria-label="delete"
+                      onClick={() => rejectFriendInvite(invite)}
+                    >
+                      <CancelIcon />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              </ListItemSecondaryAction>
             </ListItem>
           ))}
         </List>
