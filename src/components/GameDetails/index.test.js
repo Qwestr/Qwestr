e6059ffc5ed2f6cc073a15e9cb55848757a1e160
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-11-12 11:18:32 -0500

    Updated imports/ unit tests for GameDetails

--- src/components/GameDetails/index.test.js
+++ src/components/GameDetails/index.test.js
@@ -2,7 +2,12 @@ import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
 
-import { GameDetailsPage } from './index'
+import {
+  GameInviteForm,
+  GameInviteList,
+  PlayerList,
+  GameDetailsPage,
+} from './index'
 
 // Mock react-router-dom hooks
 jest.mock('react-router-dom', () => ({
@@ -17,13 +22,51 @@ configure({
 })
 
 describe('GameDetails', () => {
-  let wrapper
+  describe('GameInviteForm', () => {
+    let wrapper
 
-  beforeEach(() => {
-    wrapper = shallow(<GameDetailsPage></GameDetailsPage>)
+    beforeEach(() => {
+      wrapper = shallow(<GameInviteForm></GameInviteForm>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
   })
 
-  it('should exist!', () => {
-    expect(wrapper).toBeTruthy()
+  describe('GameInviteList', () => {
+    let wrapper
+
+    beforeEach(() => {
+      wrapper = shallow(<GameInviteList></GameInviteList>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
+  })
+
+  describe('PlayerList', () => {
+    let wrapper
+
+    beforeEach(() => {
+      wrapper = shallow(<PlayerList></PlayerList>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
+  })
+
+  describe('GameDetailsPage', () => {
+    let wrapper
+
+    beforeEach(() => {
+      wrapper = shallow(<GameDetailsPage></GameDetailsPage>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
   })
 })
