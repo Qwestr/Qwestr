1e5d5594735297d2755b057ee06eb13e373277c0
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-11-12 10:09:30 -0500

    Updated unit tests for GameDetails

--- src/components/GameDetails/index.test.js
+++ src/components/GameDetails/index.test.js
@@ -2,8 +2,15 @@ import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
 
-// import { GameDetailsPage } from './index'
-import GameDetailsPage from './index'
+import { GameDetailsPage } from './index'
+
+// Mock react-router-dom hooks
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({
+    id: 'game-id',
+  }),
+}))
 
 configure({
   adapter: new Adapter(),
