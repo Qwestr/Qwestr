f256bc8f78707aee4e175cd50d604515d83b2790
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-11-12 11:24:00 -0500

    Updated imports/ unit tests for Friends

--- src/components/Friends/index.test.js
+++ src/components/Friends/index.test.js
@@ -2,20 +2,76 @@ import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
 
-import { FriendsPage } from './index'
+import {
+  FriendInviteForm,
+  ReceivedInviteList,
+  SentInviteList,
+  FriendList,
+  FriendsPage,
+} from './index'
 
 configure({
   adapter: new Adapter(),
 })
 
 describe('Friends', () => {
-  let wrapper
+  describe('FriendInviteForm', () => {
+    let wrapper
 
-  beforeEach(() => {
-    wrapper = shallow(<FriendsPage></FriendsPage>)
+    beforeEach(() => {
+      wrapper = shallow(<FriendInviteForm></FriendInviteForm>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
   })
 
-  it('should exist!', () => {
-    expect(wrapper).toBeTruthy()
+  describe('ReceivedInviteList', () => {
+    let wrapper
+
+    beforeEach(() => {
+      wrapper = shallow(<ReceivedInviteList></ReceivedInviteList>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
+  })
+
+  describe('SentInviteList', () => {
+    let wrapper
+
+    beforeEach(() => {
+      wrapper = shallow(<SentInviteList></SentInviteList>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
+  })
+
+  describe('FriendList', () => {
+    let wrapper
+
+    beforeEach(() => {
+      wrapper = shallow(<FriendList></FriendList>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
+  })
+
+  describe('FriendsPage', () => {
+    let wrapper
+
+    beforeEach(() => {
+      wrapper = shallow(<FriendsPage></FriendsPage>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
   })
 })
