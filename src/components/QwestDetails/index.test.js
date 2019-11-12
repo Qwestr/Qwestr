aed1f04fe24af091d9c79af28405d1aea9d77f42
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-11-12 10:34:53 -0500

    Updated unit tests for QwestDetails

--- src/components/QwestDetails/index.test.js
+++ src/components/QwestDetails/index.test.js
@@ -2,8 +2,16 @@ import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
 
-// import { QwestDetails } from './index'
-import QwestDetails from './index'
+import { QwestDetailsPage } from './index'
+
+// Mock react-router-dom hooks
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({
+    id: 'qwest-id',
+    gameId: 'game-id',
+  }),
+}))
 
 configure({
   adapter: new Adapter(),
@@ -13,7 +21,7 @@ describe('QwestDetails', () => {
   let wrapper
 
   beforeEach(() => {
-    wrapper = shallow(<QwestDetails></QwestDetails>)
+    wrapper = shallow(<QwestDetailsPage></QwestDetailsPage>)
   })
 
   it('should exist!', () => {
