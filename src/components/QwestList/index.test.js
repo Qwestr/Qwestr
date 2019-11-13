ca01e76827630c1ee44a2c331c8f3c520681f042
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-11-13 12:14:22 -0500

    Updated unit tests for QwestList

--- src/components/QwestList/index.test.js
+++ src/components/QwestList/index.test.js
@@ -2,20 +2,67 @@ import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
 
-import { QwestList } from './index'
+import { QwestList, CompletedQwestList } from './index'
+
+// Setup mocks
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useEffect: jest.fn(f => f()),
+}))
 
 configure({
   adapter: new Adapter(),
 })
 
 describe('QwestList', () => {
-  let wrapper
+  describe('QwestList', () => {
+    let wrapper, authUser, firebase
 
-  beforeEach(() => {
-    wrapper = shallow(<QwestList></QwestList>)
+    beforeEach(() => {
+      authUser = {
+        uid: 'user-id',
+      }
+      firebase = {
+        userQwests: jest.fn(() => {
+          return {
+            onSnapshot: jest.fn(),
+          }
+        }),
+      }
+      wrapper = shallow(
+        <QwestList authUser={authUser} firebase={firebase}></QwestList>,
+      )
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
   })
 
-  it('should exist!', () => {
-    expect(wrapper).toBeTruthy()
+  describe('CompletedQwestList', () => {
+    let wrapper, authUser, firebase
+
+    beforeEach(() => {
+      authUser = {
+        uid: 'user-id',
+      }
+      firebase = {
+        userCompletedQwests: jest.fn(() => {
+          return {
+            onSnapshot: jest.fn(),
+          }
+        }),
+      }
+      wrapper = shallow(
+        <CompletedQwestList
+          authUser={authUser}
+          firebase={firebase}
+        ></CompletedQwestList>,
+      )
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
   })
 })
