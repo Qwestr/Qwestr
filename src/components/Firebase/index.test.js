5094a45c7db22f78d5bc0f4c211073dd0c444c84
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-11-14 09:56:12 -0500

    Working on firebase unit tests setup

--- src/components/Firebase/index.test.js
+++ src/components/Firebase/index.test.js
@@ -1,10 +1,51 @@
 import Firebase from './index'
 
+// Setup mocks
+
+function MockAuthentication() {}
+
+function MockGoogleAuthProvider() {}
+
+function MockFacebookAuthProvider() {}
+
+function MockFirebaseAuth() {
+  return new MockAuthentication()
+}
+MockFirebaseAuth.GoogleAuthProvider = MockGoogleAuthProvider
+MockFirebaseAuth.FacebookAuthProvider = MockFacebookAuthProvider
+
+jest.mock('firebase/app', () => {
+  return {
+    initializeApp: jest.fn(),
+    auth: MockFirebaseAuth,
+    // auth: jest.fn(() => {
+    //   return {
+    //     GoogleAuthProvider: jest.fn().mockImplementation(() => {
+    //       return {}
+    //     }),
+    //   }
+    // }),
+    // auth: jest.fn(),
+    // GoogleAuthProvider: jest.fn().mockImplementation(() => {
+    //   return {}
+    // }),
+    // auth: {
+    //   GoogleAuthProvider: jest.fn().mockImplementation(() => {
+    //     return {}
+    //   }),
+    //   FacebookAuthProvider: jest.fn().mockImplementation(() => {
+    //     return {}
+    //   }),
+    // },
+    firestore: jest.fn(),
+  }
+})
+
 describe('Firebase', () => {
-  let firebase = true
+  let firebase
 
   beforeEach(() => {
-    // firebase = new Firebase()
+    firebase = new Firebase()
   })
 
   it('should exist!', () => {
