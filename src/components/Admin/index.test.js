554ea03f8b9053148fbf580f86965fd5b986b937
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-11-14 14:17:44 -0500

    Updated unit tests for Admin

--- src/components/Admin/index.test.js
+++ src/components/Admin/index.test.js
@@ -2,20 +2,42 @@ import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
 
-import Admin from './index'
+import { UserList, AdminPage } from './index'
 
 configure({
   adapter: new Adapter(),
 })
 
 describe('Admin', () => {
-  let wrapper
+  describe('UserList', () => {
+    let wrapper, users
 
-  beforeEach(() => {
-    wrapper = shallow(<Admin></Admin>)
+    beforeEach(() => {
+      users = []
+      wrapper = shallow(<UserList users={users}></UserList>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
   })
 
-  it('should exist!', () => {
-    expect(wrapper).toBeTruthy()
+  describe('AdminPage', () => {
+    let wrapper, firebase
+
+    beforeEach(() => {
+      firebase = {
+        users: jest.fn(() => {
+          return {
+            onSnapshot: jest.fn(),
+          }
+        }),
+      }
+      wrapper = shallow(<AdminPage firebase={firebase}></AdminPage>)
+    })
+
+    it('should exist!', () => {
+      expect(wrapper).toBeTruthy()
+    })
   })
 })
