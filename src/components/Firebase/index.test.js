b29dc7252be527704655544ffc16b047580a884d
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-11-14 10:07:31 -0500

    Updated unit test setup for firebase

--- src/components/Firebase/index.test.js
+++ src/components/Firebase/index.test.js
@@ -1,42 +1,16 @@
 import Firebase from './index'
 
 // Setup mocks
-
-function MockAuthentication() {}
-
-function MockGoogleAuthProvider() {}
-
-function MockFacebookAuthProvider() {}
-
-function MockFirebaseAuth() {
-  return new MockAuthentication()
-}
-MockFirebaseAuth.GoogleAuthProvider = MockGoogleAuthProvider
-MockFirebaseAuth.FacebookAuthProvider = MockFacebookAuthProvider
-
 jest.mock('firebase/app', () => {
+  function MockFirebaseAuth() {
+    return jest.fn()
+  }
+  MockFirebaseAuth.GoogleAuthProvider = jest.fn()
+  MockFirebaseAuth.FacebookAuthProvider = jest.fn()
+
   return {
     initializeApp: jest.fn(),
     auth: MockFirebaseAuth,
-    // auth: jest.fn(() => {
-    //   return {
-    //     GoogleAuthProvider: jest.fn().mockImplementation(() => {
-    //       return {}
-    //     }),
-    //   }
-    // }),
-    // auth: jest.fn(),
-    // GoogleAuthProvider: jest.fn().mockImplementation(() => {
-    //   return {}
-    // }),
-    // auth: {
-    //   GoogleAuthProvider: jest.fn().mockImplementation(() => {
-    //     return {}
-    //   }),
-    //   FacebookAuthProvider: jest.fn().mockImplementation(() => {
-    //     return {}
-    //   }),
-    // },
     firestore: jest.fn(),
   }
 })
