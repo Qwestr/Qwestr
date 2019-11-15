1483caea8528c7bb6997af9a7029b2f6ff6cf905
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-11-15 14:54:37 -0500

    Updated unit tests for Firebase

--- src/components/Firebase/index.test.js
+++ src/components/Firebase/index.test.js
@@ -3,7 +3,16 @@ import Firebase from './index'
 // Setup mocks
 jest.mock('firebase/app', () => {
   const MockFirebaseAuth = () => {
-    return jest.fn()
+    return {
+      createUserWithEmailAndPassword: jest.fn(),
+      signInWithEmailAndPassword: jest.fn(),
+      signInWithPopup: jest.fn(),
+      signOut: jest.fn(),
+      sendPasswordResetEmail: jest.fn(),
+      currentUser: {
+        updatePassword: jest.fn(),
+      },
+    }
   }
   MockFirebaseAuth.GoogleAuthProvider = jest.fn()
   MockFirebaseAuth.FacebookAuthProvider = jest.fn()
@@ -24,5 +33,40 @@ describe('Firebase', () => {
 
   it('should exist!', () => {
     expect(firebase).toBeTruthy()
+  })
+
+  it('should successfully call doCreateUserWithEmailAndPassword()', () => {
+    firebase.doCreateUserWithEmailAndPassword()
+    expect(firebase.auth.createUserWithEmailAndPassword).toHaveBeenCalled()
+  })
+
+  it('should successfully call doSignInWithEmailAndPassword()', () => {
+    firebase.doSignInWithEmailAndPassword()
+    expect(firebase.auth.signInWithEmailAndPassword).toHaveBeenCalled()
+  })
+
+  it('should successfully call doSignInWithGoogle()', () => {
+    firebase.doSignInWithGoogle()
+    expect(firebase.auth.signInWithPopup).toHaveBeenCalled()
+  })
+
+  it('should successfully call doSignInWithFacebook()', () => {
+    firebase.doSignInWithFacebook()
+    expect(firebase.auth.signInWithPopup).toHaveBeenCalled()
+  })
+
+  it('should successfully call doSignOut()', () => {
+    firebase.doSignOut()
+    expect(firebase.auth.signOut).toHaveBeenCalled()
+  })
+
+  it('should successfully call doPasswordReset()', () => {
+    firebase.doPasswordReset()
+    expect(firebase.auth.sendPasswordResetEmail).toHaveBeenCalled()
+  })
+
+  it('should successfully call doPasswordUpdate()', () => {
+    firebase.doPasswordUpdate()
+    expect(firebase.auth.currentUser.updatePassword).toHaveBeenCalled()
   })
 })
