ebe1a2bc938c168a7758e8c38677e70829c59459
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-07-02 13:52:59 -0400

    Updated source names for completeBy filters to match current API

--- src/Qwests.js
+++ src/Qwests.js
@@ -39,8 +39,8 @@ const QwestFilter = (props) => (
     <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
       <SelectInput optionText="name" />
     </ReferenceInput>
-    <QuickFilter source="complete_by_yes" label="Has Complete By Date" defaultValue={true} />
-    <QuickFilter source="complete_by_no" label="No Complete By Date" defaultValue={true} />
+    <QuickFilter source="completeByYes" label="Has Complete By Date" defaultValue={true} />
+    <QuickFilter source="completeByNo" label="No Complete By Date" defaultValue={true} />
   </Filter>
 );
 
